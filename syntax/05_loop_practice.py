d71e79186192f9794c11925dfead2e211d30a153
jayhan1109 <jayhan1109@gmail.com>
2021-02-01 15:32:49 -0800

    loop_practice

---
+++ syntax/05_loop_practice.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 2 == 1:
+        print(i)
+
+sum = 0
+
+for i in range(1, n+1):
+    sum += i
+
+print(sum)
+
+
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i)
