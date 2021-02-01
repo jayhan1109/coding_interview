89fb9a514b47336befd3603f48775c9d47e3b37f
jayhan1109 <jayhan1109@gmail.com>
2021-02-01 14:45:13 -0800

    01_variable

---
+++ syntax/01_var.py
@@ -0,0 +1,16 @@
+a, b, c = 3, 2, 1
+print(a, b, c)
+
+a, b = 10, 20
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+
+a = 123458478374819283748173294827348
+print(a)
+
+print("number")
+
+print(a, b, c)
